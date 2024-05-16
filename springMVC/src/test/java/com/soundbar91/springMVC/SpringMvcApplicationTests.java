50ed28809b9f58f6216d2040dc72f2e3a6b14807
신관규 <soundbar91@gmail.com>
2024-05-16 21:33:18 +0900

    6주차 과제 프로젝트 설정

---
+++ springMVC/src/test/java/com/soundbar91/springMVC/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soundbar91.springMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
