6d4711bf3ac296b2049d78d0cd1758aea209a40f
신관규 <soundbar91@gmail.com>
2024-05-19 11:07:35 +0900

    refactor: ArticleDTO 속성 추가

--- springMVC/src/main/java/com/soundbar91/springMVC/dto/ArticleDTO.java
+++ springMVC/src/main/java/com/soundbar91/springMVC/dto/ArticleDTO.java
@@ -1,12 +1,19 @@
 package com.soundbar91.springMVC.dto;
 
+import java.time.LocalDateTime;
+
 public class ArticleDTO {
     private String title;
+    private String author;
     private String content;
+    private LocalDateTime writerDate;
 
-    public ArticleDTO(String title, String content) {
+    public ArticleDTO(String title, String author, String content) {
         this.title = title;
+        this.author = author;
         this.content = content;
+
+        this.writerDate = LocalDateTime.now();
     }
 
     public String getTitle() {
@@ -17,11 +24,27 @@ public class ArticleDTO {
         this.title = title;
     }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public String getContent() {
         return content;
     }
 
     public void setContent(String content) {
         this.content = content;
+    }
+
+    public LocalDateTime getWriterDate() {
+        return writerDate;
+    }
+
+    public void setWriterDate(LocalDateTime writerDate) {
+        this.writerDate = writerDate;
     }
 }
