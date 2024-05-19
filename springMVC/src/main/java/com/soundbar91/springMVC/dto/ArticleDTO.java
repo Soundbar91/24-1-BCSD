6ab5e0d527e1968eb512403f363022f25aa18032
신관규 <soundbar91@gmail.com>
2024-05-19 10:59:51 +0900

    refactor: Article 클래스 리펙터링

---
+++ springMVC/src/main/java/com/soundbar91/springMVC/dto/ArticleDTO.java
@@ -0,0 +1,27 @@
+package com.soundbar91.springMVC.dto;
+
+public class ArticleDTO {
+    private String title;
+    private String content;
+
+    public ArticleDTO(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
