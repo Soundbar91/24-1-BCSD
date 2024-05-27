839687543dcb078c1caf75d7af881b33e3a05f4f
신관규 <soundbar91@gmail.com>
2024-05-27 22:43:19 +0900

    refactor: Map 분리

--- springJDBC/src/main/java/com/soundbar91/springJDBC/Article/controller/PostController.java
+++ springJDBC/src/main/java/com/soundbar91/springJDBC/Article/controller/PostController.java
@@ -3,6 +3,8 @@ package com.soundbar91.springJDBC.Article.controller;
 import com.soundbar91.springJDBC.Article.dto.ResponseArticleDTO;
 import com.soundbar91.springJDBC.Article.entity.Article;
 import com.soundbar91.springJDBC.Article.service.ArticleService;
+import com.soundbar91.springJDBC.Board.dto.ResponseBoardDTO;
+import com.soundbar91.springJDBC.Board.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +12,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.sql.DataSource;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class PostController {
 
     private final ArticleService articleService;
+    private final BoardService boardService;
 
     public PostController(DataSource dataSource) {
         this.articleService = new ArticleService(dataSource);
+        this.boardService = new BoardService(dataSource);
     }
 
     @GetMapping("/posts")
     public String getPost(@RequestParam("boardId") Long boardId, Model model) {
-        Map<String, List<ResponseArticleDTO>> articleByBoardId = articleService.getArticleByBoardId(boardId);
+        ResponseBoardDTO boardById = boardService.getBoardById(boardId);
+        model.addAttribute("boardName", boardById.title());
+
+        List<ResponseArticleDTO> articleByBoardId = articleService.getArticleByBoardId(boardId);
         model.addAttribute("articles", articleByBoardId);
 
         return "post";
