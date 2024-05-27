5bee9644780b8f11f8d5b0f67ddf9903a726385a
신관규 <soundbar91@gmail.com>
2024-05-27 22:42:09 +0900

    refactor: boardRepository 속성 삭제

--- springJDBC/src/main/java/com/soundbar91/springJDBC/Article/service/ArticleService.java
+++ springJDBC/src/main/java/com/soundbar91/springJDBC/Article/service/ArticleService.java
@@ -5,46 +5,30 @@ import com.soundbar91.springJDBC.Article.dto.ResponseArticleDTO;
 import com.soundbar91.springJDBC.Article.entity.Article;
 import com.soundbar91.springJDBC.Article.repository.ArticleRepository;
 import com.soundbar91.springJDBC.Article.repository.JdbcTemplateArticleRepository;
-import com.soundbar91.springJDBC.Board.entity.Board;
-import com.soundbar91.springJDBC.Board.repository.BoardRepository;
-import com.soundbar91.springJDBC.Board.repository.JdbcTemplateBoardRepository;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
-    private final BoardRepository boardRepository;
 
     public ArticleService(DataSource dataSource) {
         this.articleRepository = new JdbcTemplateArticleRepository(dataSource);
-        this.boardRepository = new JdbcTemplateBoardRepository(dataSource);
     }
 
-    public Map<String, List<ResponseArticleDTO>> getArticleByBoardId(Long boardId) {
-        String boardName = boardRepository.getBoardById(boardId)
-                .map(Board::getName)
-                .orElse(null);
+    public List<ResponseArticleDTO> getArticleByBoardId(Long boardId) {
+        List<ResponseArticleDTO> responseArticleDTOList = new ArrayList<>();
+        List<Article> articlesByBoard = articleRepository.getArticlesByBoard(boardId);
 
-        if (boardName == null) return null;
-        else {
-            Map<String, List<ResponseArticleDTO>> result = new HashMap<>();
+        for (Article article : articlesByBoard)
+            responseArticleDTOList.add(ResponseArticleDTO.of(article));
 
-            List<ResponseArticleDTO> responseArticleDTOList = new ArrayList<>();
-            List<Article> articlesByBoard = articleRepository.getArticlesByBoard(boardId);
-
-            for (Article article : articlesByBoard) responseArticleDTOList.add(ResponseArticleDTO.of(article));
-
-            result.put(boardName, responseArticleDTOList);
-            return result;
-        }
+        return responseArticleDTOList;
     }
 
     public ResponseArticleDTO getArticleById(Long articleId) {
