550fe3c589fbdc2c50754e880c278a34c8be615b
신관규 <soundbar91@gmail.com>
2024-05-27 22:41:38 +0900

    feat: getArticleByBoardId 메소드 추가

--- springJDBC/src/main/java/com/soundbar91/springJDBC/Article/controller/ArticleController.java
+++ springJDBC/src/main/java/com/soundbar91/springJDBC/Article/controller/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @RestController
 @RequestMapping("/articles")
@@ -19,11 +20,17 @@ public class ArticleController {
         this.articleService = new ArticleService(dataSource);
     }
 
+    @GetMapping
+    public ResponseEntity<List<ResponseArticleDTO>> getArticleByBoardId(@RequestParam("boardId") Long boardId) {
+        List<ResponseArticleDTO> articles = articleService.getArticleByBoardId(boardId);
+        return ResponseEntity.status(HttpStatus.OK).body(articles);
+    }
+
     @GetMapping("/{id}")
     public ResponseEntity<ResponseArticleDTO> getArticleById(@PathVariable("id") Long id) {
         ResponseArticleDTO responseArticleDTO = articleService.getArticleById(id);
         return responseArticleDTO != null ?
-                ResponseEntity.status(HttpStatus.OK).body(responseArticleDTO) : ResponseEntity.notFound().build();
+                ResponseEntity.ok(responseArticleDTO) : ResponseEntity.notFound().build();
     }
 
     @PostMapping
@@ -36,12 +43,12 @@ public class ArticleController {
     public ResponseEntity<ResponseArticleDTO> updateArticle(@PathVariable("id") Long id, @RequestBody RequestArticleDTO requestArticleDTO) {
         ResponseArticleDTO responseArticleDTO = articleService.updateArticle(id, requestArticleDTO);
         return responseArticleDTO == null ?
-                ResponseEntity.status(HttpStatus.NOT_FOUND).build() : ResponseEntity.status(HttpStatus.OK).body(responseArticleDTO);
+                ResponseEntity.notFound().build() : ResponseEntity.ok(responseArticleDTO);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteArticle(@PathVariable("id") Long id) {
         return articleService.deleteArticle(id) ?
-                ResponseEntity.status(HttpStatus.NO_CONTENT).build() : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+                ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 }
